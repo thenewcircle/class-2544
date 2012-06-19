e749ff3b9cc7873aecbedb80f48386560ea81b72
Marko Gargenta <marko@marakana.com>
2012-06-19 16:40:31 -0700

    Added AsyncTask

--- CiscoYamba/src/com/cisco/yamba/StatusActivity.java
+++ CiscoYamba/src/com/cisco/yamba/StatusActivity.java
@@ -1,10 +1,13 @@
 package com.cisco.yamba;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.marakana.android.yamba.clientlib.YambaClient;
 
@@ -20,23 +23,48 @@ public class StatusActivity extends Activity {
 		// Find the views
 		statusText = (TextView) findViewById(R.id.status_update_text);
 	}
-	
+
 	/** Called when update button is clicked on. */
 	public void onUpdateButtonClick(View v) {
 		final String status = statusText.getText().toString();
 		
-		new Thread() {
-			public void run() {
-				try {
-					YambaClient yambaClient = new YambaClient("student", "password");
-					yambaClient.updateStatus(status);
-				} catch (Exception e) {
-					Log.e("StatusActivity", "CRASHED!", e);
-					e.printStackTrace();
-				}
-			}
-		}.start();
+		new StatusUpdateTask().execute(status);
+	}
+
+	/** Posts the status update in a separate task. */
+	class StatusUpdateTask extends AsyncTask<String, Void, String> {
+		Dialog dialog;
 		
-		Log.d("StatusActivity", "onUpdateButtonClick'ed with text: "+status);
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			dialog = new Dialog(StatusActivity.this);
+			dialog.setTitle("Posting status update...");
+			dialog.show();
+		}
+
+		/** Task that happens on a separate thread. */
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				YambaClient yambaClient = new YambaClient("student", "password");
+				yambaClient.updateStatus(params[0]); // could take some time
+				return "Status update posted successfully";
+			} catch (Exception e) {
+				Log.e("StatusActivity", "CRASHED!", e);
+				e.printStackTrace();
+				return "Failed to post the status update";
+			}
+		}
+
+		/** Called once doInBackground() is complete. */
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			dialog.dismiss();
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG)
+					.show();
+		}
+
 	}
 }
