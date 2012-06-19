66caa2588ac1a10e76814fed4196e1096a7583f4
Marko Gargenta <marko@marakana.com>
2012-06-19 12:32:52 -0700

    Added UI bindings to Java

--- CiscoYamba/src/com/cisco/yamba/StatusActivity.java
+++ CiscoYamba/src/com/cisco/yamba/StatusActivity.java
@@ -2,21 +2,24 @@ package com.cisco.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
 
 public class StatusActivity extends Activity {
-	Button updateButton;
 	TextView statusText;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.status);
+
+		// Find the views
+		statusText = (TextView) findViewById(R.id.status_update_text);
+	}
 	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.status);
-        
-        // Find the views
-        updateButton = (Button) findViewById(R.id.status_update_button);
-        statusText = (TextView) findViewById(R.id.status_update_text);
-    }
+	/** Called when update button is clicked on. */
+	public void onUpdateButtonClick(View v) {
+		// TODO
+	}
 }
