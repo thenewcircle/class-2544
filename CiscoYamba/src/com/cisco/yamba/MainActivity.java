8e27549eba181674e8890180f95489ec84b36228
Marko Gargenta <marko@marakana.com>
2012-06-20 14:15:12 -0700

    Implemented onOptionsItemSelected()

--- CiscoYamba/src/com/cisco/yamba/MainActivity.java
+++ CiscoYamba/src/com/cisco/yamba/MainActivity.java
@@ -1,8 +1,10 @@
 package com.cisco.yamba;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -12,12 +14,26 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.main);
 	}
 
+	/** Called first time menu is needed. */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
-	
-	
+
+	/** Called every time an options item is selected. */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.item_prefs:
+			
+			return true;
+		case R.id.item_status_update:
+			startActivity( new Intent(this, StatusActivity.class) );
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
 }
