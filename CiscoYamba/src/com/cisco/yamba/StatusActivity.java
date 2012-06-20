ec2f3d67036dcbf5e4d5908cee67ac5141b107f8
Marko Gargenta <marko@marakana.com>
2012-06-20 11:08:17 -0700

    Implemented TextWatcher

--- CiscoYamba/src/com/cisco/yamba/StatusActivity.java
+++ CiscoYamba/src/com/cisco/yamba/StatusActivity.java
@@ -2,8 +2,11 @@ package com.cisco.yamba;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -12,7 +15,9 @@ import android.widget.Toast;
 import com.marakana.android.yamba.clientlib.YambaClient;
 
 public class StatusActivity extends Activity {
-	TextView statusText;
+	private static final int MAX_LENGTH = 140;
+	private TextView statusText, counterText;
+	private int defaultColor;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -22,19 +27,23 @@ public class StatusActivity extends Activity {
 
 		// Find the views
 		statusText = (TextView) findViewById(R.id.status_update_text);
+		statusText.addTextChangedListener( new MyTextWatcher() );
+		counterText = (TextView) findViewById(R.id.status_update_counter_text);
+		counterText.setText( Integer.toString(MAX_LENGTH-statusText.length()) );
+		defaultColor = counterText.getTextColors().getDefaultColor();
 	}
 
 	/** Called when update button is clicked on. */
 	public void onUpdateButtonClick(View v) {
 		final String status = statusText.getText().toString();
-		
+
 		new StatusUpdateTask().execute(status);
 	}
 
 	/** Posts the status update in a separate task. */
 	class StatusUpdateTask extends AsyncTask<String, Void, String> {
 		Dialog dialog;
-		
+
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
@@ -66,5 +75,33 @@ public class StatusActivity extends Activity {
 					.show();
 		}
 
+	}
+
+	// --- Part of being TextWatcher --- //
+	class MyTextWatcher implements TextWatcher {
+		@Override
+		public void afterTextChanged(Editable s) {
+			int count = MAX_LENGTH - s.length();
+			counterText.setText( Integer.toString(count) );
+			
+			// Change the color
+			if(count < 30) {
+				counterText.setTextColor( Color.RED );
+				counterText.setTextScaleX(2);
+			} else {
+				counterText.setTextColor( defaultColor );
+				counterText.setTextScaleX(1);
+			}
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+		}
+
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+		}
 	}
 }
