61972bfb63d941152d8bd52df07bdf1c3d9232eb
Marko Gargenta <marko@marakana.com>
2012-06-20 17:35:49 -0700

    Refactored to use dynamic fragments

--- CiscoYamba/src/com/cisco/yamba/PrefsActivity.java
+++ CiscoYamba/src/com/cisco/yamba/PrefsActivity.java
@@ -1,6 +1,8 @@
 package com.cisco.yamba;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class PrefsActivity extends Activity {
@@ -8,7 +10,16 @@ public class PrefsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.prefs_activity);
+		setContentView(R.layout.activity_with_fragment);
+		
+		if (savedInstanceState == null) {
+			FragmentManager fragmentManager = getFragmentManager();
+			FragmentTransaction transaction = fragmentManager
+					.beginTransaction();
+			PrefsFragment fragment = new PrefsFragment();
+			transaction.add(R.id.fragment_container, fragment);
+			transaction.commit();
+		}
 	}
 
 }
