4b36a0b2c32cdf4c3055d371aa6ba13ace80f21c
Marko Gargenta <marko@marakana.com>
2012-06-22 12:10:06 -0700

    Implemented LoaderManager

--- CiscoYamba/src/com/cisco/yamba/TimelineFragment.java
+++ CiscoYamba/src/com/cisco/yamba/TimelineFragment.java
@@ -1,34 +1,38 @@
 package com.cisco.yamba;
 
 import android.app.ListFragment;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.view.View;
+import android.widget.CursorAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorAdapter.ViewBinder;
 import android.widget.TextView;
 
-public class TimelineFragment extends ListFragment {
-	private static final String[] FROM = { StatusContract.Columns.USER, StatusContract.Columns.TEXT, StatusContract.Columns.CREATED_AT };
-	private static final int[] TO = { R.id.text_user, R.id.text_text, R.id.text_created_at };
+public class TimelineFragment extends ListFragment implements
+		LoaderCallbacks<Cursor> {
+	private static final String[] FROM = { StatusContract.Columns.USER,
+			StatusContract.Columns.TEXT, StatusContract.Columns.CREATED_AT };
+	private static final int[] TO = { R.id.text_user, R.id.text_text,
+			R.id.text_created_at };
 	private SimpleCursorAdapter adapter;
-	private Cursor cursor;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		setEmptyText("Loading data...");
 
-		cursor = getActivity().getContentResolver().query(
-				StatusContract.CONTENT_URI, null, null, null,
-				StatusContract.SORT_ORDER);
-
-		adapter = new SimpleCursorAdapter(getActivity(), R.layout.row, cursor,
-				FROM, TO);
+		adapter = new SimpleCursorAdapter(getActivity(), R.layout.row, null,
+				FROM, TO, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
 		adapter.setViewBinder(new TimelineViewBinder());
 
 		setListAdapter(adapter);
+
+		getLoaderManager().initLoader(0, null, this);
 	}
 
 	/** Handles custom binding of data to view. */
@@ -43,9 +47,28 @@ public class TimelineFragment extends ListFragment {
 			long timestamp = cursor.getLong(columnIndex);
 			CharSequence relativeTime = DateUtils
 					.getRelativeTimeSpanString(timestamp);
-			((TextView)view).setText(relativeTime);
-			
+			((TextView) view).setText(relativeTime);
+
 			return true;
 		}
 	}
+
+	// --- LoaderManager.LoaderCallbacks ---
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(getActivity(), StatusContract.CONTENT_URI,
+				null, null, null, StatusContract.SORT_ORDER);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		adapter.swapCursor(cursor);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		adapter.swapCursor(null);
+	}
+
 }
