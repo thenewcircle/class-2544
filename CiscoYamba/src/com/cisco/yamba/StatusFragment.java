4629ead36cf2c58c9e2ed0ec27093de6288eabbc
Marko Gargenta <marko@marakana.com>
2012-06-22 12:46:06 -0700

    Added Location

--- CiscoYamba/src/com/cisco/yamba/StatusFragment.java
+++ CiscoYamba/src/com/cisco/yamba/StatusFragment.java
@@ -2,9 +2,13 @@ package com.cisco.yamba;
 
 import android.app.Dialog;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -21,11 +25,15 @@ import android.widget.Toast;
 
 import com.marakana.android.yamba.clientlib.YambaClient;
 
-public class StatusFragment extends Fragment implements OnClickListener {
+public class StatusFragment extends Fragment implements OnClickListener,
+		LocationListener {
 	private static final int MAX_LENGTH = 140;
+	private static final String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
 	private TextView statusText, counterText;
 	private ImageButton updateButton;
 	private int defaultColor;
+	private LocationManager locationManager;
+	private Location location;
 
 	/** Called by activity when it's ready for fragment to create its view. */
 	@Override
@@ -44,7 +52,25 @@ public class StatusFragment extends Fragment implements OnClickListener {
 				.findViewById(R.id.status_update_button);
 		updateButton.setOnClickListener(this);
 
+		// Get location
+		locationManager = (LocationManager) getActivity().getSystemService(
+				Context.LOCATION_SERVICE);
+		location = locationManager.getLastKnownLocation(LOCATION_PROVIDER);
+
 		return view;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		locationManager.requestLocationUpdates(LOCATION_PROVIDER, 10000, 100,
+				this);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		locationManager.removeUpdates(this);
 	}
 
 	/** Called when update button is clicked on. */
@@ -78,12 +104,18 @@ public class StatusFragment extends Fragment implements OnClickListener {
 				YambaClient yambaClient = new YambaClient(username, password);
 				if (server != null && server.length() > 0)
 					yambaClient.setApiRoot(server);
-				yambaClient.updateStatus(params[0]); // could take some time
+
+				if (location != null) {
+					yambaClient.updateStatus(params[0], location.getLatitude(),
+							location.getLongitude()); // could take some time
+				} else {
+					yambaClient.updateStatus(params[0]);
+				}
 
 				// Send broadcast that there may be new data on the server
 				getActivity().sendBroadcast(
 						new Intent("com.cisco.yamba.REFRESH_ACTION"));
-				
+
 				return "Status update posted successfully";
 			} catch (Exception e) {
 				Log.e("StatusActivity", "CRASHED!", e);
@@ -129,4 +161,25 @@ public class StatusFragment extends Fragment implements OnClickListener {
 				int count) {
 		}
 	}
+
+	// --- Callbacks for LocatoinManager
+	@Override
+	public void onLocationChanged(Location location) {
+		this.location = location;
+		Log.d("StatusFragment", String.format("location: %f, %f",
+				location.getLatitude(), location.getLongitude()));
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
+
 }
